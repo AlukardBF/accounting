2b8607056cd397b2315996788011294abe4fbee0
AlukardBF <alukard.develop@gmail.com>
2018-12-17 02:01:48 +0400

    Сделаны навбары
    Удалены ненужные css и js

---
+++ public/js/side-nav.js
@@ -0,0 +1,5 @@
+$('.btn-expand-collapse').click(function(e) {
+    $('.navbar-primary').toggleClass('collapsed');
+    $('#nav-expand-icon').toggleClass('fa-angle-left');
+    $('#nav-expand-icon').toggleClass('fa-angle-right');
+});
