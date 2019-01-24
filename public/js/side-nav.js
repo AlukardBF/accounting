e67ee29ebe02dec4248f63f6f3ef520dfa349adf
AlukardBF <alukard.develop@gmail.com>
2019-01-24 21:01:50 +0400

    Удален sidebar, поправлена верстка

--- public/js/side-nav.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    $('.btn-expand-collapse').click(function(e) {
-        $('.navbar-primary').toggleClass('collapsed');
-        $('#nav-expand-icon').toggleClass('fa-angle-left');
-        $('#nav-expand-icon').toggleClass('fa-angle-right');
-    });
-});
